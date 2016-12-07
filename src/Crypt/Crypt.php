78ea6b1e3ece94340b2d21d549ebc4e6f2e42290
Jaroslav Hranička <hranicka@outlook.com>
2016-12-07 10:04:26 +0100

    Crypt: phpDoc

--- src/Crypt/Crypt.php
+++ src/Crypt/Crypt.php
@@ -9,8 +9,20 @@ namespace Markette\Gopay\Crypt;
 interface Crypt
 {
 
+	/**
+	 * Encrypt 3DES to HEX.
+	 * @param string $data
+	 * @param string $secureKey
+	 * @return string
+	 */
 	public function encrypt($data, $secureKey);
 
+	/**
+	 * Decrypt HEX 3DES to string.
+	 * @param string $data
+	 * @param string $secureKey
+	 * @return string
+	 */
 	public function decrypt($data, $secureKey);
 
 }
